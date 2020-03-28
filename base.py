1c8db062136faf43b5c4d0bfab19bbf5de09ada0
TakuNakai <nakai.taku@outlook.jp>
2020-03-28 09:16:44 +0900

    MC.py

--- base.py
+++ base.py
@@ -15,22 +15,6 @@ from matplotlib import animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.mplot3d import Axes3D
 
-from OCC.Display.SimpleGui import init_display
-from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
-from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
-from OCC.Core.gp import gp_XYZ
-from OCC.Core.gp import gp_Lin
-from OCC.Core.gp import gp_Mat, gp_GTrsf, gp_Trsf
-from OCC.Core.TopoDS import TopoDS_Shape
-from OCC.Core.TopLoc import TopLoc_Location
-from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeSphere
-from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeWire
-from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_GTransform
-from OCCUtils.Construct import make_box, make_line, make_wire
-from OCCUtils.Construct import make_plane, make_polygon
-from OCCUtils.Construct import point_to_vector, vector_to_point
-from OCCUtils.Construct import dir_to_vec, vec_to_dir
-
 import logging
 logging.getLogger('matplotlib').setLevel(logging.ERROR)
 
@@ -85,7 +69,7 @@ class SetDir (object):
 
 
 class PlotBase(SetDir):
-    
+
     def __init__(self, aspect="equal"):
         SetDir.__init__(self)
         self.dim = 2
@@ -149,7 +133,7 @@ class plot2d (PlotBase):
     def __init__(self, aspect="equal"):
         PlotBase.__init__(self)
         self.dim = 2
-        #self.new_2Dfig(aspect=aspect)
+        # self.new_2Dfig(aspect=aspect)
         self.new_fig(aspect=aspect)
 
     def add_axs(self, row=1, col=1, num=1, aspect="auto"):
@@ -266,105 +250,6 @@ class plot3d (PlotBase):
         #self.axs.set_xlim3d(-10, 10)
         #self.axs.set_ylim3d(-10, 10)
         #self.axs.set_zlim3d(-10, 10)
-
-
-def pnt_trf_vec(pnt=gp_Pnt(), vec=gp_Vec()):
-    v = point_to_vector(pnt)
-    v.Add(vec)
-    return vector_to_point(v)
-
-
-def set_trf(ax1=gp_Ax3(), ax2=gp_Ax3()):
-    trf = gp_Trsf()
-    trf.SetTransformation(ax2, ax1)
-    return trf
-
-
-def set_loc(ax1=gp_Ax3(), ax2=gp_Ax3()):
-    trf = set_trf(ax1, ax2)
-    loc = TopLoc_Location(trf)
-    return loc
-
-
-def trsf_scale(axs=gp_Ax3(), scale=1):
-    trf = gp_Trsf()
-    trf.SetDisplacement(gp_Ax3(), axs)
-    return trf
-
-
-def gen_ellipsoid(axs=gp_Ax3(), rxyz=[10, 20, 30]):
-    sphere = BRepPrimAPI_MakeSphere(gp_Ax2(), 1).Solid()
-    loc = set_loc(gp_Ax3(), axs)
-    mat = gp_Mat(
-        rxyz[0], 0, 0,
-        0, rxyz[1], 0,
-        0, 0, rxyz[2]
-    )
-    gtrf = gp_GTrsf(mat, gp_XYZ(0, 0, 0))
-    ellips = BRepBuilderAPI_GTransform(sphere, gtrf).Shape()
-    ellips.Location(loc)
-    return ellips
-
-
-class plotocc (SetDir):
-
-    def __init__(self):
-        self.display, self.start_display, self.add_menu, self.add_functionto_menu = init_display()
-        SetDir.__init__(self)
-
-    def show_box(self, axs=gp_Ax3(), lxyz=[100, 100, 100]):
-        box = make_box(*lxyz)
-        ax1 = gp_Ax3(
-            gp_Pnt(-lxyz[0] / 2, -lxyz[1] / 2, -lxyz[2] / 2),
-            gp_Dir(0, 0, 1)
-        )
-        trf = gp_Trsf()
-        trf.SetTransformation(axs, gp_Ax3())
-        trf.SetTransformation(ax1, gp_Ax3())
-        box.Location(TopLoc_Location(trf))
-        self.display.DisplayShape(axs.Location())
-        self.show_axs_pln(axs, scale=lxyz[0])
-        self.display.DisplayShape(box, transparency=0.7)
-
-    def show_pnt(self, xyz=[0, 0, 0]):
-        self.display.DisplayShape(gp_Pnt(*xyz))
-
-    def show_ball(self, scale=100, trans=0.5):
-        shape = BRepPrimAPI_MakeSphere(scale).Shape()
-        self.display.DisplayShape(shape, transparency=trans)
-
-    def show_ellipsoid(self, axs=gp_Ax3(), rxyz=[10., 10., 10.], trans=0.5):
-        shape = gen_ellipsoid(axs, rxyz)
-        self.display.DisplayShape(shape, transparency=trans, color="BLUE")
-        return shape
-
-    def show_axs_pln(self, axs=gp_Ax3(), scale=100):
-        pnt = axs.Location()
-        dx = axs.XDirection()
-        dy = axs.YDirection()
-        dz = axs.Direction()
-        vx = dir_to_vec(dx).Scaled(1 * scale)
-        vy = dir_to_vec(dy).Scaled(2 * scale)
-        vz = dir_to_vec(dz).Scaled(3 * scale)
-
-        pnt_x = pnt_trf_vec(pnt, vx)
-        pnt_y = pnt_trf_vec(pnt, vy)
-        pnt_z = pnt_trf_vec(pnt, vz)
-        self.display.DisplayShape(pnt)
-        self.display.DisplayShape(make_line(pnt, pnt_x), color="RED")
-        self.display.DisplayShape(make_line(pnt, pnt_y), color="GREEN")
-        self.display.DisplayShape(make_line(pnt, pnt_z), color="BLUE")
-
-    def show_plane(self, axs=gp_Ax3(), scale=100):
-        pnt = axs.Location()
-        vec = dir_to_vec(axs.Direction())
-        pln = make_plane(pnt, vec, -scale, scale, -scale, scale)
-        self.display.DisplayShape(pln)
-
-    def show(self):
-        self.display.FitAll()
-        self.display.View.Dump(self.tempname + ".png")
-        self.start_display()
 
 
 class LineDrawer(object):
